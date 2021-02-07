709a5f5f64fc14b1ce542f7f4cf27e80e21cd1cd
mozibrishu <mozib.diu@gmail.com>
2021-02-07 13:13:28 +0600

    Mostly Done: need some recheck

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 document.getElementById("search-btn").addEventListener("click", function () {
-    showWarning(""); // removing warning Text
+    // removing warning text and hiding mealDetails section
+    showWarning("");
     document.getElementById("meal-details").style.display = 'none';
-    
+
     const inputMealName = document.getElementById("input-meal-name").value;
     document.getElementById("input-meal-name").value = "";
+
+    // trimming empty spaces
     const mealName = inputMealName.trim();
 
     if (mealName === "") {
@@ -15,38 +18,43 @@ document.getElementById("search-btn").addEventListener("click", function () {
                 if (data.meals === null) {
                     showWarning(`No meal found with name \"${mealName}\". Please try again.`)
                 } else {
-                    processData(data.meals);
+                    displayFoundMeals(data.meals);
                 }
             })
     }
 })
 
-function processData(meals) {
+function showWarning(warningText) {
+    document.getElementById("warning-text").innerText = warningText;
+}
+
+
+function displayFoundMeals(meals) {
     // clearing previous search result
     document.getElementById("meal-list").innerHTML = "";
-    
+
     meals.forEach(meal => {
-        const mealDiv = document.createElement("div");        
-        mealDiv.innerHTML  = `
+        const mealDiv = document.createElement("div");
+        mealDiv.innerHTML = `
         <div onclick='mealDetails("${meal.idMeal}")' class="meal-card">
             <img src="${meal.strMealThumb}" class="meal-image">
             <h5 class="meal-title">${meal.strMeal}</h5>
         </div>
-        `
+        `;
         document.getElementById("meal-list").appendChild(mealDiv);
     });
 }
 
-function mealDetails(mealId){
+function mealDetails(mealId) {
     console.log(mealId);
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
-    .then(res => res.json())
-    .then(data =>{
-        displayMealDetails(data.meals[0]);
-    })
+        .then(res => res.json())
+        .then(data => {
+            displayMealDetails(data.meals[0]);
+        })
 }
 
-function displayMealDetails(meal){
+function displayMealDetails(meal) {
     document.getElementById("meal-details-display").innerHTML = `
     <div class="text-center">
         <img src="${meal.strMealThumb}" class="meal-details-image">
@@ -58,28 +66,29 @@ function displayMealDetails(meal){
 
         </ul>
     </div>
-    `
+    `;
+
+    // Displaying meals instructions.
     document.getElementById("instruction-display").innerHTML = `
     <p class="instructions">${meal.strInstructions}</p>
-    `
-    console.log(meal);
-    for(let i=1;i<=20;i++){
-        let ingredient = 'strIngredient'+i;
-        let quantity = 'strMeasure'+i;
-        if(meal[ingredient] === ""|| meal[ingredient]==null){
+    `;
+
+    // Displaying Ingredients.
+    for (let i = 1; i <= 20; i++) {
+        let ingredient = 'strIngredient' + i;
+        let quantity = 'strMeasure' + i;
+
+        if (meal[ingredient] === "" || meal[ingredient] == null) {
             break;
         }
+
         const li = document.createElement("li");
         li.innerHTML = `
         <li><i class="icon-color fas fa-check-square"></i> ${meal[quantity]} ${meal[ingredient]}</li>
         `;
         document.getElementById("ingredient-list").appendChild(li)
     }
+
     document.getElementById("meal-details").style.display = "block";
-    document.getElementById("meal-details").scrollIntoView({behavior: "smooth"});
-}
-
-
-function showWarning(warningText) {
-    document.getElementById("warning-text").innerText = warningText;
+    document.getElementById("meal-details").scrollIntoView({ behavior: "smooth" });
 }
